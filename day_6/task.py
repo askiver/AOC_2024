f5c34080c10ac866884050f6995eb2be144f5f16
Ask Iver <sylteaskiver@yahoo.com>
2024-12-10 03:00:20 +0100

    Day 6 complete

--- day_6/task.py
+++ day_6/task.py
@@ -1,9 +1,8 @@
+from tqdm import tqdm
 import numpy as np
 map = []
 for line in open("input.txt", 'r'):
     map.append([sign for sign in line.strip()])
-
-map = np.array(map)
 
 # Task 1
 max_y, max_x = len(map)-1, len(map[0])-1
@@ -16,28 +15,28 @@ dir_dict = {
 
 pos_array = np.zeros((max_y+1, max_x+1))
 # Find initial position
-pos = next((y,x) for y, row in enumerate(map) for x, elem in enumerate(row) if elem == '^')
-pos = np.array(pos, dtype=int)
+org_pos = next((y,x) for y, row in enumerate(map) for x, elem in enumerate(row) if elem == '^')
+pos = np.array(org_pos, dtype=int)
 current_dir = np.array([-1,0], dtype=int)
 while True:
-    if pos[0] > max_y or pos[1] > max_x or np.any(pos < 0):
+    y,x = pos
+    if y > max_y or x > max_x or np.any(pos < 0):
         break
-    elif map[tuple(pos)] == '#':
+    elif map[y][x] == '#':
         pos = pos - current_dir
-        current_dir = dir_dict[tuple(current_dir)]
+        current_dir = dir_dict[*current_dir]
     else:
-        pos_array[tuple(pos)] = 1
+        pos_array[y][x] = 1
         pos = pos + current_dir
 
 
 print(int(np.sum(pos_array)))
+traversed_path = np.where(pos_array == 1)
 
 
 # Task 2
 
 pos_array = np.zeros((max_y+1, max_x+1, 4))
-org_pos = next((y,x) for y, row in enumerate(map) for x, elem in enumerate(row) if elem == '^')
-org_pos = np.array(org_pos)
 current_dir = np.array([-1,0])
 total_loops = 0
 
@@ -48,38 +47,43 @@ key_dict = {
     (0,-1): 3,
 }
 
-def check_loop(pos, dir, map_copy):
-    copy_pos_array = pos_array.copy()
-    current_pos, current_dir = pos, dir
-    while not (current_pos[0] + current_dir[0] > max_y or current_pos[1] + current_dir[1] > max_x or current_pos[0] + current_dir[0] < 0 or current_pos[1] + current_dir[1] < 0):
-        next_pos = pos + dir
-        dict_key = key_dict[tuple(current_dir)]
-        if map_copy[tuple(next_pos)] == '#':
-            current_dir = dir_dict[tuple(current_dir)]
-        elif copy_pos_array[tuple(current_pos)][dict_key] > 0:
+def check_bounds(pos, dir):
+    y,x = pos + dir
+    if y > max_y or x > max_x or np.any(pos < 0):
+        return False
+    return True
+
+def step(pos, dir):
+    next_pos = pos + dir
+    y,x = next_pos
+    if map[y][x] == "#":
+        next_dir = dir_dict[*dir]
+        return pos, next_dir
+    return next_pos, dir
+
+def check_loop():
+    pos = np.array(org_pos)
+    pos_array = np.zeros((max_y+1, max_x+1, 4))
+    current_dir = np.array([-1,0])
+    while check_bounds(pos, current_dir):
+        y, x = pos
+        next_pos, next_dir = step(pos, current_dir)
+        dict_key = key_dict[*next_dir]
+        if pos_array[y,x,dict_key] == 1:
             return True
         else:
-            copy_pos_array[tuple(current_pos)][dict_key] = 1
-            current_pos += current_dir
+            if not np.array_equal(next_pos, pos):
+                pos_array[y,x,dict_key] = 1
+                pos = next_pos
+            current_dir = next_dir
     return False
 
-pos = org_pos
-while True:
-    dict_key = key_dict[tuple(current_dir)]
-    next_pos = pos + current_dir
-    if next_pos[0] > max_y or next_pos[1] > max_x or np.any(pos < 0):
-        break
-    elif map[tuple(next_pos)] == '#':
-        current_dir = dir_dict[tuple(current_dir)]
-    elif np.array_equal(next_pos, org_pos):
-        pos_array[tuple(pos)][dict_key] = 1
-        pos += current_dir
-    else:
-        map_copy = map.copy()
-        map_copy[tuple(pos+current_dir)] = '#'
-        result = check_loop(pos.copy(), dir_dict[tuple(current_dir)], map_copy)
-        pos_array[tuple(pos)][dict_key] = 1
-        pos += current_dir
-        total_loops += result
+for index in tqdm(list(zip(traversed_path[0], traversed_path[1]))):
+    y,x = index
+    if map[y][x] == "^":
+        continue
+    map[y][x] = "#"
+    total_loops += check_loop()
+    map[y][x] = "."
 
 print(total_loops)
